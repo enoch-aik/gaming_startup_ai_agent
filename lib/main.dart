241775d4ea6e611579c0553c6bf947b12c2c478c
Enoch <enoch_aik@icloud.com>
2025-03-18 17:00:23 +0100

    - added chat user interface

--- lib/main.dart
+++ lib/main.dart
@@ -43,16 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: ChatScreen(),
-      floatingActionButton: FloatingActionButton(
+      /*floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),
+      ),*/
     );
   }
 }
